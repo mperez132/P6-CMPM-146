c08c01237d73f985e096e06faec002594b25310a
Ian Richardson <ierichar@ucsc.edu>
2021-11-08 00:08:45 -0800

    upto part5 finished, fixing part6

--- src/models/dropout_model.py
+++ src/models/dropout_model.py
@@ -1,9 +1,32 @@
+from operator import mod
+from keras.backend import relu, sigmoid, variable
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
+from tensorflow.python.keras.layers.core import Dropout
+
 
 # Define your dropout model here
 model = models.Sequential()
+
+model.add(layers.Convolution2D(32, kernel_size=(7, 7), activation=relu, padding='same', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D(2, 3))
+model.add(layers.Convolution2D(64, kernel_size=(7, 7), activation=relu, padding='same'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Convolution2D(128, kernel_size=(7, 7), activation=relu, padding='same'))
+model.add(layers.MaxPooling2D(2, 1))
+model.add(layers.Convolution2D(256, kernel_size=(7, 7), activation=relu, padding='same'))
+model.add(layers.MaxPooling2D(3, 1))
+model.add(layers.Convolution2D(512, kernel_size=(7, 7), activation=relu, padding='same'))
+model.add(layers.MaxPooling2D(3, 3))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation=relu))
+model.add(Dropout(0.5))
+model.add(layers.Dense(1, activation=sigmoid))
+
+print(len(model.weights))
+print(model.layers)
+print(model.summary())
 
 # Then, call model.compile()
 model.compile(
